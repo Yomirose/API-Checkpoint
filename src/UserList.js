2f53ba0a3a2574c309b6579af5c964aa8d2c62d7
Yomirose <roselinefayomi10@yahoo.comcleargit config --global user.email roselinefayomi10@yahoo.com>
2024-08-07 16:57:38 +0100

    First commit

---
+++ src/UserList.js
@@ -0,0 +1,36 @@
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUsers(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the user data!", error);
+      });
+  }, []);
+
+  return (
+    <div className="user-list">
+      <h1>User List</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+}
+
+export default UserList;
